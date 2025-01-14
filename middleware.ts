2aa383e16ec3fcad4933bc4c665482cef01d6fb8
suraj bisht <suryabisht00@protonmail.com>
2025-01-14 17:11:24 +0530

    now you can easily protect the some or all routes using middleware , check comments for better understanding.

--- middleware.ts
+++ middleware.ts
@@ -19,20 +19,17 @@ export const config = {
 };
 
 
+
 // // protect only those routes that require specific permissions
 
 // import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// const isProtectedRoute = createRouteMatcher(['/admin(.*)']) // Protect all routes under /admin you ca add more routes here
+// const isProtectedRoute = createRouteMatcher(['/client(.*)', '/forum(.*)'])
 
 // export default clerkMiddleware(async (auth, req) => {
-//   // Restrict admin routes to users with specific permissions
-//   if (isProtectedRoute(req)) {
-//     await auth.protect((has) => {
-//       return has({ permission: 'org:admin:example1' }) || has({ permission: 'org:admin:example2' })
-//     })
-//   }
+//   if (isProtectedRoute(req)) await auth.protect()
 // })
+
 // export const config = {
 //   matcher: [
 //     // Skip Next.js internals and all static files, unless found in search params
@@ -41,4 +38,3 @@ export const config = {
 //     '/(api|trpc)(.*)',
 //   ],
 // }
-
